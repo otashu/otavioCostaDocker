a3016f8c6606d52dce78b22a46902d75902cada5
otashu <otaviocosta23212@gmail.com>
2023-05-12 15:41:03 -0300

    terraform v1.5

--- main.tf
+++ main.tf
@@ -1,17 +1,68 @@
 provider "aws" {
-  access_key = "AKIA6NQ6LDRTMJWKYGHA"
-  secret_key = "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"
   region     = "us-east-1"
 }
 
-resource "aws_security_group" "sg-terraform" {
-  name        = "sgterraform"
-  description = "sgterraform"
+resource "aws_security_group" "sg-bastion" {
+  name        = "sgbastion"
+  description = "sgbastion"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+resource "aws_security_group" "sg-principal" {
+  name        = "sgprincipal"
+  description = "sgprincipal"
 
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
+    security_groups = [aws_security_group.sg-bastion.id]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 111
+    to_port     = 111
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 111
+    to_port     = 111
+    protocol    = "udp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -21,6 +72,7 @@ resource "aws_security_group" "sg-terraform" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
 
   egress {
     from_port   = 0
@@ -30,23 +82,39 @@ resource "aws_security_group" "sg-terraform" {
   }
 }
 
-
-
-
-resource "aws_instance" "terraform" {
-  ami           = "ami-0c94855ba95c71c99"
+resource "aws_instance" "bastion" {
+  ami           = "ami-06a0cd9728546d178"
   instance_type = "t2.micro"
   key_name      = "Charizard"
   tags = {
-    Name       = "PB UNIVEST URI"
+    Name       = "PB UNIVEST URI - bastion"
     CostCenter = "C092000004"
     Project    = "PB UNIVEST URI"
   }
 
-  vpc_security_group_ids = [aws_security_group.sg-terraform.id]
+  vpc_security_group_ids = [aws_security_group.sg-bastion.id]
 
   volume_tags = {
-    Name       = "PB UNIVEST URI"
+    Name       = "PB UNIVEST URI - bastion"
+    CostCenter = "C092000004"
+    Project    = "PB UNIVEST URI"
+  }
+}
+
+resource "aws_instance" "principal" {
+  ami           = "ami-06a0cd9728546d178"
+  instance_type = "t2.micro"
+  key_name      = "Charizard"
+  tags = {
+    Name       = "PB UNIVEST URI - principal"
+    CostCenter = "C092000004"
+    Project    = "PB UNIVEST URI"
+  }
+
+  vpc_security_group_ids = [aws_security_group.sg-principal.id]
+
+  volume_tags = {
+    Name       = "PB UNIVEST URI - principal"
     CostCenter = "C092000004"
     Project    = "PB UNIVEST URI"
   }
